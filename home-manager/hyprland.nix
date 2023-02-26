e80c6c18955d54c16b3afe935482ddb57ea908bf
Hello, this is my username <124476172+theswagdoctor@users.noreply.github.com>
2023-02-25 23:57:54 -0500

    fix emoji nvim remove all grammars

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -8,7 +8,7 @@
  programs.foot.enable = true;
  programs.waybar.enable = true;
  home.packages = with pkgs; [
- font-awesome swayidle swaybg wlsunset wl-clipboard mako wev 
+ font-awesome swayidle swaybg wlsunset wl-clipboard mako rofi-wayland rofimoji wev 
  ];
 
 home.sessionVariables = {
