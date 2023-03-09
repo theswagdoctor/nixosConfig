f8c93dacc98415b1873c509540e4194a2a699da5
Hello, this is my username <124476172+theswagdoctor@users.noreply.github.com>
2023-03-09 17:17:39 -0500

    change wttr location

--- home-manager/config/waybar/scripts/waybar-wttr.py
+++ home-manager/config/waybar/scripts/waybar-wttr.py
@@ -58,7 +58,7 @@ WEATHER_CODES = {
 data = {}
 
 
-weather = requests.get("https://wttr.in/?format=j1").json()
+weather = requests.get("https://wttr.in/pwm?format=j1").json()
 
 
 def format_time(time):
